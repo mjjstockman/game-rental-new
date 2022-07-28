5f2727b707b9279eca5c24e14954f13b9fd6d13f
mjjstockman <mjjstockman@gmail.com>
2022-07-28 13:20:54 +0000

    Remove unwanted whitespace in run.py

--- run.py
+++ run.py
@@ -16,8 +16,47 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("game_rental_new")
 
-stock = SHEET.worksheet('stock')
+# stock = SHEET.worksheet('stock')
 
-data = stock.get_all_values()
+# data = stock.get_all_values()
 
-print(data)
+# print(data)
+def validate_tel_num(tel_num):
+    try:
+        if (len(tel_num)) != 11:
+            raise ValueError(
+                f"Phone number must be 11 digits long, you entered {tel_num}, which is {len(tel_num)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again")
+    # all nums
+    # length is 11
+    # print (len(tel_num))
+
+
+
+def get_user_tel_num():
+    tel_num = input("Please enter your phone number:\n").strip()
+    validate_tel_num(tel_num)   
+
+
+def get_user_name():
+    """
+    Welcomes the customer and gets their name and phone number 
+    """
+    print("Hi! Welcome to Super Games!\n")
+    name = input("Please enter your full name:\n")
+
+    print(f"You have entered {name}.\n")
+    print("Is this correct?\n")
+    confirm = input("Enter Y for yes, N for No\n")
+    confirm_strip_lcase = confirm.strip().lower()
+    if confirm_strip_lcase == "n":
+        print("No worries, try again!\n")
+        get_user_name()
+    elif confirm_strip_lcase == "y":
+        get_user_tel_num()
+
+
+get_user_name()
+
